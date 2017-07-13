25d313e21ae9b152ac7cd7c9ffde7e9879fdc6d4
陈小俊 <chenxj07@mingyuanyun.com>
2017-07-13 23:26:51 +0800

    启动服务区

---
+++ express.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+//指定静态文件的位置
+app.use('/', express.static(__dirname + '/dist')); 
+
+//监听端口号
+server.listen(4010);
